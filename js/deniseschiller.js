c05dc9bc1ee5a5cc5d9d04038382a07461da2946
Rocio Paez <yo@rociopaez.com>
2015-08-26 12:03:17 -0500

    Set global steps

--- js/deniseschiller.js
+++ js/deniseschiller.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
   $('a[href="' + document.location.pathname + '"]').parent().addClass('active');
+
+  // var seconds = 5000;
+  // var step = 0;
+  // var limit = 5;
+  //
+  // $("body").addClass("image-"+step).fadeIn(500);
+  //
+  // setInterval(function() {
+  //   $("#Background").fadeOut(500,function(){
+  //      $(this).removeClass("image-"+step);
+  //      step = (step > limit) ? 0 : step + 1;
+  //     $("#Background").addClass("image-"+step).fadeIn(500);
+  //   });
+  // },seconds);
 });
