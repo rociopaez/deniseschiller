2a51a4531335ea16d84d16c50a3144aecdc2b16e
Rocio Paez <yo@rociopaez.com>
2015-09-06 15:37:33 -0500

    Add slide pictures

--- js/deniseschiller.js
+++ js/deniseschiller.js
@@ -1,17 +1,49 @@
 $(document).ready(function() {
+  var $body   = $("body");
+  var $footer = $("footer");
+  var prefix  = 'step-';
+
+  var transitionBackground = function() {
+    var current = BackgroundCounter.current;
+    var next    = current + 1;
+    var total   = BackgroundCounter.total;
+
+    $body.removeClass(prefix + current);
+    next = (next >= total) ? 0 : next;
+    $body.addClass(prefix + next);
+ 
+    BackgroundCounter.current = next;
+    $footer.html(next + 1);
+  };
+
   $('a[href="' + document.location.pathname + '"]').parent().addClass('active');
 
-  // var seconds = 5000;
-  // var step = 0;
-  // var limit = 5;
-  //
-  // $("body").addClass("image-"+step).fadeIn(500);
-  //
-  // setInterval(function() {
-  //   $("#Background").fadeOut(500,function(){
-  //      $(this).removeClass("image-"+step);
-  //      step = (step > limit) ? 0 : step + 1;
-  //     $("#Background").addClass("image-"+step).fadeIn(500);
-  //   });
-  // },seconds);
+  if (BackgroundCounter.total > 0) {
+    var seconds  =10000;
+    var interval = window.setInterval(transitionBackground, seconds);
+
+    $body.addClass(prefix + '0');
+
+    $body.on("keyup", function(e) {
+      if (e.keyCode == 37) {
+        window.clearInterval(interval);
+
+        current = BackgroundCounter.current; 
+        $body.removeClass(prefix + current);
+
+        if (current == 0) {
+          BackgroundCounter.current = BackgroundCounter.total - 2;
+        } else if (current == 1) {
+          BackgroundCounter.current = BackgroundCounter.total - 1;
+        } else {
+          BackgroundCounter.current = current - 2;
+        };
+        transitionBackground();
+      } else if (e.keyCode == 39) {
+        clearInterval(interval);
+
+        transitionBackground();
+      };
+    });
+  }
 });
